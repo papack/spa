a4cdcef91a7f713cdc199f6d00093b25c06440b4
Matthias Steiner <m@kjpjm.de>
2025-11-26 18:18:36 +0100

    add simple effect

---
+++ examples/9-effect/main.tsx
@@ -0,0 +1,21 @@
+import { render, h, fragment, signal, time, effect } from "../../src";
+
+render(<MyComponent />, document.getElementById("root")!);
+
+async function MyComponent() {
+  const [value, setValue] = signal(0);
+  const [conent, setContent] = signal("");
+  effect(value, async (v) => {
+    await setContent(() => `value is ${v}`);
+  });
+
+  setInterval(() => {
+    setValue((prev) => prev + 1);
+  }, time.SECOND);
+
+  return (
+    <>
+      <div>{conent}</div>
+    </>
+  );
+}
